a414fe62e69d3a4be3cbb5cdd2ce6ac185614012
Feroz Shah <shah2207087@stud.kuet.ac.bd>
2025-11-10 18:42:07 +0600

    initial commit

---
+++ src/main/java/com/example/shah_2207087_gpacalculator/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.shah_2207087_gpacalculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/com/example/shah_2207087_gpacalculator/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.shah_2207087_gpacalculator;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
