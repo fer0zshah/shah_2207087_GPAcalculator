9c48499b9eab0dad195e188fc3829df7e1fe94cd
Md Feroz Shah <shah2207087@stud.kuet.ac.bd>
2025-11-17 03:40:04 +0600

    scen1 perfectly connected to scene2

--- src/main/java/com/example/shah_2207087_gpacalculator/scene2controller.java
+++ src/main/java/com/example/shah_2207087_gpacalculator/scene2controller.java
@@ -1,4 +1,59 @@
 package com.example.shah_2207087_gpacalculator;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
 public class scene2controller {
+
+    @FXML
+    private TextField CodeFeild;
+
+    @FXML
+    private TextField CreditFeild;
+
+    @FXML
+    private TextField NameFeild;
+
+    @FXML
+    private TextField Teacher1Feild;
+
+    @FXML
+    private Label currentCreditLabel;
+
+    @FXML
+    private ComboBox<?> gradebox;
+
+    @FXML
+    private BorderPane rootPane;
+
+    @FXML
+    private TextField teacher2Feild;
+
+    @FXML
+    private TextField totalCreditField;
+
+    @FXML
+    void addCourse(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backToHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void calculateButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clearFeilds(ActionEvent event) {
+
+    }
+
 }
