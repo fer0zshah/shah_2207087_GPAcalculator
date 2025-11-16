3db9861ad7de2077dfc4191119bf0bfd97c33660
Md Feroz Shah <shah2207087@stud.kuet.ac.bd>
2025-11-16 20:52:35 +0600

    Homepage Created

--- src/main/java/com/example/shah_2207087_gpacalculator/HelloController.java
+++ src/main/java/com/example/shah_2207087_gpacalculator/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.shah_2207087_gpacalculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader; // Import FXMLLoader
+import javafx.scene.Node;       // Import Node
+import javafx.scene.Parent;     // Import Parent
+import javafx.scene.Scene;       // Import Scene
+import javafx.scene.control.Button;
+import javafx.stage.Stage;       // Import Stage
+
+import java.io.IOException; // Import IOException
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button start;
+
+    @FXML
+    void clickStart(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("scene2.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("GPA Calculator");
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/example/shah_2207087_gpacalculator/scene2controller.java
@@ -0,0 +1,4 @@
+package com.example.shah_2207087_gpacalculator;
+
+public class scene2controller {
+}
