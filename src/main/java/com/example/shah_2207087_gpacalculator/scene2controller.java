01164b4041726f9cbdc2f58a9d94d89e3b6587db
Md Feroz Shah <shah2207087@stud.kuet.ac.bd>
2025-11-17 17:20:37 +0600

    Addcourse and Clear feild button implemented

--- src/main/java/com/example/shah_2207087_gpacalculator/scene2controller.java
+++ src/main/java/com/example/shah_2207087_gpacalculator/scene2controller.java
@@ -9,7 +9,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+//import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -17,7 +23,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
+//requires javafx.controls;
 
 
 
@@ -61,11 +67,35 @@ public class scene2controller implements Initializable {
     }
 
 
-
-
     @FXML
     void addCourse(ActionEvent event) {
+        String courseName = NameFeild.getText();
+        String courseCode = CodeFeild.getText();
+        String creditStr = CreditFeild.getText();
+        String teacher1 = Teacher1Feild.getText();
+        String teacher2 = teacher2Feild.getText();
+        String grade = gradebox.getValue();
+        double credit= Double.parseDouble(creditStr);;
 
+        if (courseName.isEmpty() || courseCode.isEmpty() || creditStr.isEmpty()) {
+            showError("Input Error", "Course Name, Code, and Credit cannot be empty.");
+            return;
+        }
+        try {
+            credit = Double.parseDouble(creditStr);
+            if (credit <= 0) throw new NumberFormatException();
+        } catch (NumberFormatException e) {
+            showError("Input Error", "Credit must be a positive number");
+            return;
+        }
+
+        Course newCourse = new Course(courseName, courseCode, credit, teacher1, teacher2, grade);
+
+        courseList.add(newCourse);
+
+        currentTotalCredits += credit;
+        currentCreditLabel.setText(String.format("%.1f", currentTotalCredits));
+        clearFeilds(null);
     }
 
     @FXML
@@ -80,6 +110,18 @@ public class scene2controller implements Initializable {
 
     @FXML
     void clearFeilds(ActionEvent event) {
-
+        NameFeild.clear();
+        CodeFeild.clear();
+        CreditFeild.clear();
+        Teacher1Feild.clear();
+        teacher2Feild.clear();
+        gradebox.setValue("A+");
+    }
+    private void showError(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
