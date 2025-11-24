3a6c92daee70f350ce066e533c88ab50592def45
Md Feroz Shah <shah2207087@stud.kuet.ac.bd>
2025-11-25 00:22:40 +0600

    Table created

--- src/main/java/com/example/shah_2207087_gpacalculator/database.java
+++ src/main/java/com/example/shah_2207087_gpacalculator/database.java
@@ -1,4 +1,55 @@
 package com.example.shah_2207087_gpacalculator;
 
-public class database {
+import java.sql.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class database{
+
+    private static final String DB_URL = "jdbc:sqlite:gpa_data.db";
+
+    public static void initializeDB() {
+        String sql = "CREATE TABLE IF NOT EXISTS courses (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT," +
+                "code TEXT," +
+                "credit REAL," +
+                "teacher1 TEXT," +
+                "teacher2 TEXT," +
+                "grade TEXT" +
+                ")";
+
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Database has been initialized.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addCourse(Course course) {
+        String sql = "INSERT INTO courses(name, code, credit, teacher1, teacher2, grade) VALUES(?,?,?,?,?,?)";
+
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, course.getCourseName());
+            pstmt.setString(2, course.getCourseCode());
+            pstmt.setDouble(3, course.getCredit());
+            pstmt.setString(4, course.getTeacher1());
+            pstmt.setString(5, course.getTeacher2());
+            pstmt.setString(6, course.getGrade());
+
+            pstmt.executeUpdate();
+            System.out.println("Course saved to database.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example.shah_2207087_gpacalculator {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
